31059a1f8381e071bef831e811721946c9054e6f
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-07-07 00:13:55 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-const  App = () => {
+import { BrowserRouter } from "react-router-dom";
+
+import { Navbar, Hero, About, Experience, Tech, Works, Feedbacks, Contact, StarsCanvas } from './components';
+
+const App = () => {
   return (
-    <div>
-      3D Portfolio
-    </div>
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
