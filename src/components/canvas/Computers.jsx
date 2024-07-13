54f761f69cfe12767e6e39e13a3cd7c5c2e7e1fb
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-07-13 14:48:46 +0300

    Update Computers.jsx

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Suspense, useState, useEffect } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
+import CanvasLoader from '../Loader';
 
 const Computers = () => {
+  const computer = useGLTF ('./descktop_pc/scene.gltf');
+
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      <pointLight intensity={1} />
+      <primitive object={computer.scene} />
+    </mesh>
   )
 }
 
-export default Computers
+export default Computers;
