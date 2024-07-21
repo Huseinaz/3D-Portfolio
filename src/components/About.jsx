a537d8a2db4003be261a5e3a114984a2eed04897
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-07-21 16:15:55 +0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,6 +17,13 @@ const About = () => {
           Overview.
         </h2>
       </motion.div>
+
+      <motion.p
+        variants={fadeIn('', '', 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        I&apos;m a software engineer Passionate about technology with expertise in full stack web development and a strong foundation in software engineering. Proficient in Flutter, React JS, and Laravel. Experienced in collaborative environments, adept at problem solving, and committed to continuous learning. Let&apos;s connect and explore opportunities for innovative projects.
+      </motion.p>
     </>
   )
 }
