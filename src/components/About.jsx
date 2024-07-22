596383e26ca7cab0638723c877e95c073773bf8f
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-07-22 18:14:07 +0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
 import { Tilt } from 'react-tilt';
 import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
 import { services } from '../constants';
 import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
 
 const ServicCard = ({ index, title, icon }) => {
   return (
@@ -57,4 +57,4 @@ const About = () => {
   )
 }
 
-export default About;
+export default SectionWrapper(About, 'about');
